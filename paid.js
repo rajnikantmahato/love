042d3239eb7001bd24b4da6c676aae931a60c427
Rajni KAnt mahato <58804492+rajnikantmahato@users.noreply.github.com>
2022-05-13 05:15:05 +0530

    Add files via upload

---
+++ paid.js
@@ -0,0 +1,8 @@
+var kathakali = prompt("Enter your Key : ")
+if (kathakali=="user0"){
+    var simplehacker  = prompt("Enter Victim Phone no +91 : ")
+}
+else{
+    alert("your not a paid user")
+    window.location.assign("index.html")
+}
