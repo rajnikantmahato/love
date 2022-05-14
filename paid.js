fd9c93430343c02e83878b5b6b5c25bd4aeadfea
Rajni KAnt mahato <58804492+rajnikantmahato@users.noreply.github.com>
2022-05-14 15:57:43 +0530

    Update paid.js

--- paid.js
+++ paid.js
@@ -1,6 +1,5 @@
 var kathakali = prompt("Enter your Key : ")
 if (kathakali=="user1"){
-    //var simplehacker  = prompt("Enter Victim Phone no +91 : ")
 window.location.assign("https://paidserver2.w3spaces.com/indenew_enc.html")
 }
 else{
