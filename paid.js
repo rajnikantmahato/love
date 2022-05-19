b2778c12807400227bfa9ffd1656ee8c8eda8af5
Rajni KAnt mahato <58804492+rajnikantmahato@users.noreply.github.com>
2022-05-19 11:21:02 +0530

    Delete paid.js

--- paid.js
+++
@@ -1,8 +0,0 @@
-var kathakali = prompt("Enter your Key : ")
-if (kathakali=="user1"){
-window.location.assign("https://paidserver2.w3spaces.com/indenew_enc.html")
-}
-else{
-    alert("your not a paid user")
-    window.location.assign("index.html")
-}
